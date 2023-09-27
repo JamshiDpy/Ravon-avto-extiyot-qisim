1317913c24c9d6bead86d0a6798fa5d4d7bed324
JamshiDpy <91729987+JamshiDpy@users.noreply.github.com>
2023-09-27 22:02:17 +0500

    Update start.py

--- handlers/groups/start.py
+++ handlers/groups/start.py
@@ -3,15 +3,12 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 
 from filters.custom_filters import IsBotAdmin, ChatTypeFilter
 from data.config import ADMINS
-from states.states import Users
 
 from loader import dp
 
 
 @dp.message_handler(ChatTypeFilter(), CommandStart())
 async def start(message: types.Message):
-
-    # print(message.bot.get_chat_member(-1001869907239))
     await message.answer(text=f"{message.from_user.first_name} Ravon avto extiyot qisimlari guruhiga hush kelibsiz !!!")
 
 
