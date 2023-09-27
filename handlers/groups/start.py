d3bc52e1459a9887a84d2c9eb601e9ddbf8758f1
JamshiDpy <91729987+JamshiDpy@users.noreply.github.com>
2023-09-28 00:35:41 +0500

    Update start.py

--- handlers/groups/start.py
+++ handlers/groups/start.py
@@ -15,5 +15,5 @@ async def start(message: types.Message):
 @dp.message_handler(ChatTypeFilter(), content_types=types.ContentType.NEW_CHAT_MEMBERS)
 async def new_chat_member(message: types.Message):
     if message.new_chat_members[0].is_bot:
-        await message.bot.ban_chat_member(chat_id=-1554079401, user_id=message.new_chat_members[0].id)
+        await message.bot.ban_chat_member(chat_id=-1001554079401, user_id=message.new_chat_members[0].id)
         print(message.new_chat_members[0].first_name, 'nomli bot guruhdan chiqarb yuborldi !!!')
