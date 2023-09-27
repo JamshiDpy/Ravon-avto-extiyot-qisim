c3d2a70bb9c797fe68d7a64ee0b81e4206eed5d4
JamshiDpy <91729987+JamshiDpy@users.noreply.github.com>
2023-09-27 22:01:10 +0500

    Update anti_rek.py

--- handlers/groups/anti_rek.py
+++ handlers/groups/anti_rek.py
@@ -11,7 +11,7 @@ async def delete_rek(message: types.Message):
     print(message)
     if message.entities:
         if message.entities[0].type == 'url' or 'mention':
-            await message.bot.delete_message(chat_id=-1001869907239, message_id=message.message_id)
+            await message.bot.delete_message(chat_id=-1554079401, message_id=message.message_id)
             await message.answer(f"{message.from_user.first_name} bu guruhga havola tarqatish taqiqlanadi ❌❌❌")
 
 
@@ -19,5 +19,5 @@ async def delete_rek(message: types.Message):
 async def media(message: types.Message):
     if 'caption_entities' in message:
         if message.caption_entities[0].type == 'url' or 'text_url' or 'text_link' or 'mention':
-           await message.bot.delete_message(chat_id=-1001869907239, message_id=message.message_id)
+           await message.bot.delete_message(chat_id=-1554079401, message_id=message.message_id)
            await message.answer(text=f"{message.from_user.first_name} Bu guruhga bog'lama biriktirilga medialar yuborish tqiqlanadi ❌❌❌")
