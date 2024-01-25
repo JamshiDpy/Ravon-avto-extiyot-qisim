d4f5113eb2fe11d9a1e77c6368e9610852b0c990
JamshiDpy <91729987+JamshiDpy@users.noreply.github.com>
2024-01-25 08:22:16 +0500

    Update anti_rek.py

--- handlers/groups/anti_rek.py
+++ handlers/groups/anti_rek.py
@@ -20,4 +20,4 @@ async def media(message: types.Message):
     if 'caption_entities' in message:
         if message.caption_entities[0].type == 'url' or 'text_url' or 'text_link' or 'mention':
            await message.bot.delete_message(chat_id=-1001554079401, message_id=message.message_id)
-           await message.answer(text=f"{message.from_user.first_name} Bu guruhga bog'lama biriktirilga medialar yuborish tqiqlanadi ❌❌❌")
+           await message.answer(text=f"{message.from_user.first_name} Bu guruhga bog'lama biriktirilga medialar yuborish taqiqlanadi ❌❌❌")
